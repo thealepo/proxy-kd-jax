da72cd31f967b18a35d01cdbf1ed63e747bfece0
TheAlepo <sanchezc.alexe@gmail.com>
2026-06-28 13:28:13 -0400

    "proxy" generate func"

--- proxy_alignment.py
+++ proxy_alignment.py
@@ -3,11 +3,24 @@ import jax
 import jax.numpy as jnp
 from flax import nnx
 
-def generation(model , input_ids , rng):
-    ...
+def generate(model , input_ids , rng):
+    # wnat to return the probability distribution and hard output
+    # NOTE: must replace black box model with a true API (irl, we wont get a prob distribution form a black box model)
 
-def preference_loss(input_ids , y_winner , y_loser):
-    pass
+    logits = model(input_ids)  # [batch , seq_len , vocab_size]
+
+    # log probs
+    log_probs = jax.nn.log_softmax(logits , axis=-1)
+    # token
+    next_token = jax.random.categorical(
+        rng , logits[: , -1 , :] , axis=1
+    )
+
+    return next_token , log_probs
+
+
+def preference_loss(teacher_model , proxy_model , input_ids , y_winner , y_loser):
+    ...
 
 def proxy_alignment(teacher_model , proxy_model , input_ids , rng):
     pass
