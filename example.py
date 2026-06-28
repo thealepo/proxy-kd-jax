a1c3bd192bc26684a069b83683678ed880c85d2e
TheAlepo <sanchezc.alexe@gmail.com>
2026-06-28 17:59:12 -0400

    prompt maker

--- example.py
+++ example.py
@@ -7,3 +7,10 @@ from transformer import TransformerConfig , CausalLanguageModel
 from utils import BlackBoxTeacher , autoregressive_generation
 import proxy_alignment as phase1
 import student_kd as phase2
+
+# globabl prompt creator
+def make_prompts(rng , num_batches , batch , prompt_len , vocab_size):
+    keys = jax.random.split(rng , num_batches)
+    return [
+        jax.random.randint(key , (batch,prompt_len) , 0 , vocab_size , dtype=jnp.int32) for key in keys
+    ]
