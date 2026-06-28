982a4d3c7c608bcd02e9adcca80b3e1ad46b1807
TheAlepo <sanchezc.alexe@gmail.com>
2026-06-28 14:30:18 -0400

    new token log prob func

--- proxy_alignment.py
+++ proxy_alignment.py
@@ -5,6 +5,24 @@ from flax import nnx
 
 BETA = 0.1
 
+def get_token_log_probs(model , input_ids , response):
+    # input ids: [batch , prompt_len] | response: [batch , response_len]
+    full_seq = jnp.concatenate([input_ids,response] , axis=-1)  # [batch , prompt+response]
+    logits = model(full_seq)
+    log_probs = jax.nn.log_softmax(logits , axis=-1)  # [batch , prompt+response , vocab_size]
+
+    # Next-token pred
+    targets = full_seq[: , 1:] # [batch , p+r-1]
+    log_probs = log_probs[: , :-1 , :]  # [batch , p+r-1 , vocab_size]
+
+    token_log_probs = jnp.take_along_axis(
+        log_probs , targets[... , None] , axis=-1
+    ).squeeze(-1)  # [batch , p+r-1]
+
+    # Keep only the response
+    response_len = response.shape[-1]
+    return token_log_probs[: , -response_len:]  # [batch , response_len]
+
 def generate(model , input_ids , rng):
     # wnat to return the probability distribution and hard output
     # NOTE: must replace black box model with a true API (irl, we wont get a prob distribution form a black box model)
