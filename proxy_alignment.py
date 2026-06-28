20f346b3b930653d1f3f6652a71b1a938a8bcce7
TheAlepo <sanchezc.alexe@gmail.com>
2026-06-28 14:32:12 -0400

    split of token and log_probs

--- proxy_alignment.py
+++ proxy_alignment.py
@@ -27,15 +27,9 @@ def generate(model , input_ids , rng):
     # wnat to return the probability distribution and hard output
     # NOTE: must replace black box model with a true API (irl, we wont get a prob distribution form a black box model)
 
-    logits = model(input_ids)  # [batch , seq_len , vocab_size]
-
-    # log probs
-    log_probs = jax.nn.log_softmax(logits , axis=-1)
-    # token
-    next_token = jax.random.categorical(
-        rng , logits[: , -1 , :] , axis=1
-    )
-
+    logits = model(input_ids)  # [batch , prompt_len , vocab_size]
+    log_probs = jax.nn.log_softmax(logits , axis=1)
+    next_token = jax.random.categorical(rng , logits[: , -1 , :] , axis=-1)
     return next_token , log_probs
 
 
